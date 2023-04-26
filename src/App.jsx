b914d40e9dbe16ee4082f9d374237e1bfa931cc3
Florencio Britez <florenciobritez49@gmail.com>
2023-04-26 21:47:50 +0000

    Add tailwind to project

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,48 @@ function App() {
         </div>
         <div>
           <label htmlFor="tip-percentage">Select Tip %</label>
+          <div>
+            <input type="text" value="5%" />
+          </div>
+          <div>
+            <input type="text" value="10%" />
+          </div>
+          <div>
+            <input type="text" value="15%" />
+          </div>
+          <div>
+            <input type="text" value="25%" />
+          </div>
+          <div>
+            <input type="text" value="50%" />
+          </div>
+          <div className="custom">
+            <input type="text" />
+          </div>
         </div>
         <div>
           <label htmlFor="people">Number of people</label>
           <input type="number" />
         </div>
       </form>
-      <div></div>
+      <div>
+        <div>
+          <div>
+            <div>
+              <h2>Tip Amount</h2>
+              <p>/ person</p>
+            </div>
+            <span>
+              $0
+            </span>
+          </div>
+          <div>
+            <h2>Total</h2>
+            <p>/ person</p>
+          </div>
+        </div>
+        <button>Reset</button>
+      </div>
     </div>
   );
 }
